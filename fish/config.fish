a44d799b45311c13423e1ec408cc964d0bfca7d6
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-01-23 22:14:07 +0200

    Refactored fish configuration; use fish-git package, as fish 2.4.2 has problem with vi key bindings

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
 # Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# set -q XDG_DATA_HOME
+#   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+#   or set -gx OMF_PATH "$HOME/.local/share/omf"
+#
+# # Load Oh My Fish configuration.
+# source $OMF_PATH/init.fish
 # omf theme shellder
-omf theme bobthefish
+# omf theme bobthefish
 
 # disable virtualenv prompt, already taken care by theme
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,2 +1,1 @@
-fish_vi_key_bindings
 fzf_key_bindings
